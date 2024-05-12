cb5644980aab1fa61247b35ee1c957064cdc810c
Nick <nickphilomath@gmail.com>
2024-05-12 11:32:29 +0500

    add check_match function to path class; add echo function ;

--- app/main.py
+++ app/main.py
@@ -13,8 +13,12 @@ def home(request) -> str:
     return response
 
 def hello(request) -> str:
-    response = "HTTP/1.1 200 OK \r\n\r\n hello world".encode()
+    response = "HTTP/1.1 200 OK \r\n\r\nhello world".encode()
     return response
+
+def echo(request, url_vars_dic):
+    msg = url_vars_dic.get('msg', '')
+    return f"HTTP/1.1 200 OK \r\n\r\n{msg}".encode()
 
 def handle404(request) -> str:
     response = "HTTP/1.1 404 Not Found \r\n\r\n".encode()
@@ -31,29 +35,49 @@ class Path:
         self.url = url
         self.view = view
 
+    def check_match(self, url):
+        pu_list = self.url.split('/') # path urls list
+        ru_list = url.split('/') # request urls list
+
+        url_vars_dic = {}  # we store url variable values here and later pass it to view
+
+        for i in range(len(pu_list)):
+            if ':' in pu_list[i]: # if url is varialble
+                url_varname = pu_list[i]
+                url_varname = url_varname[1:] # remove first character colon
+                url_vars_dic[url_varname] = ru_list[i]
+                continue
+            elif pu_list[i] == ru_list[i]:
+                continue
+            else:
+                return {}, False
+            
+        return url_vars_dic, True
+
 
 urlpatterns = [
     Path('/', home),
     Path('/hello', hello),
+    Path('/echo/:msg', echo),
 ]
 
 # this function returns specific function that 
 # is responsible to handle given url
 def get_url_view(url):
     for path in urlpatterns:
-        if path.url == url:
-            return path.view
+        url_vars_dic, path_is_match = path.check_match(url)
+        if path_is_match:
+            if len(url_vars_dic) == 0:
+                return path.view
+            else:
+                return lambda request: path.view(request, url_vars_dic)
         
     return handle404
 
 
 def handle_server_request(request):
     start_line, headers, body, *other = request.split('\r\n')
-
     method, req_url, http_version, *other = start_line.split(' ')
-
-    # print(start_line, headers, body, other, sep=" ### ")
-    # print(method, url, http_v, sep=" ### ")
 
     view = get_url_view(req_url)
     response = view(request)
